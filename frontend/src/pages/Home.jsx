e428944ea1380a3acf6b008da8c0ccd95e5f80e0
ckcks1997 <ckcks1997@gmail.com>
2024-08-18 21:31:33 +0900

    도메인/SSL 인증서 적용, nginx 프록시 적용

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
 
   const handleSearch = async () => {
     try {
-      const response = await fetch(`http://127.0.0.1:8000/search_business?business_name=${searchTerm}`);
+      const response = await fetch(`https://company-color.kro.kr/api/search_business?business_name=${searchTerm}`);
       const data = await response.json();
       console.log(data)
       navigate('/result', { state: { searchResult: data } });
